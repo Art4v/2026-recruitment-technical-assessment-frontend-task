26dc824d8a93882bf381147770a1ebe76508f3e8
Art4v <aarav_20@outlook.com>
2026-02-27 01:17:58 +1100

    added required rooms and styling to roomcard grid

--- frontend-assessment/src/pages/RoomPage.jsx
+++ frontend-assessment/src/pages/RoomPage.jsx
@@ -10,6 +10,15 @@ import filter_alt from '../assets/filter_alt.png';
 import filter_list from '../assets/filter_list.png';
 
 import agsm from '../assets/agsm.webp';
+import ainsworth from '../assets/ainsworth.webp';
+import anitab from '../assets/anitab.webp';
+import biologicalScience from '../assets/biologicalScience.webp';
+import biologicalScienceWest from '../assets/biologicalScienceWest.webp';
+import blockhouse from '../assets/blockhouse.webp';
+import businessSchool from '../assets/businessSchool.webp';
+import civilBuilding from '../assets/civilBuilding.webp';
+import colombo from '../assets/colombo.webp';
+import cseBuilding from '../assets/cseBuilding.webp';
 
 // import usestate
 import { useState } from 'react';
@@ -20,7 +29,16 @@ function RoomPage() {
   
   // rooms stored here
   const rooms = [
-    {id: 1, name: 'AGSM', availability: '5 rooms available', url: agsm}
+    {id: 1, name: 'AGSM', availability: '9 rooms available', url: agsm},
+    {id: 2, name: 'Ainsworth Building', availability: '16 rooms available', url: ainsworth},
+    {id: 3, name: 'Anita B Lawrence Centre', availability: '44 rooms available', url: anitab},
+    {id: 4, name: 'Biological Sciences', availability: '6 rooms available', url: biologicalScience},
+    {id: 5, name: 'Biological Science (West)', availability: '8 rooms available', url: biologicalScienceWest},
+    {id: 6, name: 'Blockhouse', availability: '42 rooms available', url: blockhouse},
+    {id: 7, name: 'Business School', availability: '18 rooms available', url: businessSchool},
+    {id: 8, name: 'Civil Engineering Building', availability: '8 rooms available', url: civilBuilding},
+    {id: 9, name: 'Colombo Building', availability: '5 rooms available', url: colombo},
+    {id: 10, name: 'Computer Science & Eng (K17)', availability: '7 rooms available', url: cseBuilding}
   ]
   
   // search function
