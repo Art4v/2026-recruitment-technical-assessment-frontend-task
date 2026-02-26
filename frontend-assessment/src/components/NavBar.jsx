04a1a73b46c9c04a7cfb39a76d9d1db315bccab2
Art4v <aarav_20@outlook.com>
2026-02-27 01:57:56 +1100

    implemented component state management for logo

--- frontend-assessment/src/components/NavBar.jsx
+++ frontend-assessment/src/components/NavBar.jsx
@@ -6,30 +6,45 @@ import '../css/NavBar.css';
 
 // imports for images
 import freeRoomsLogo from '../assets/freeRoomsLogo.png';
+import freeRoomsDoorClosed from '../assets/freeroomsDoorClosed.png'
+
 import search from '../assets/search.png';
 import grid_view from '../assets/grid_view.png';
 import free_room from '../assets/free_room.png';
 import map from '../assets/map.png';
 import dark_mode from '../assets/dark_mode.png';
 
+// import usestate
+import { useState } from 'react';
 
 // navbar
 function NavBar() {
-return <nav className='navbar'>
-  <div className='navbar-brand'>
-    <Link to='/'>
-      <img src={freeRoomsLogo} alt='freeRoomsLogo'/>
-	  Freerooms
-    </Link>
-  </div>
-  <div className='navbar-links'>
-    <Link to='/' className='nav-link'><img src={search} alt='search' /></Link>
-    <Link to='/' className='nav-link'><img src={grid_view} alt='search' /></Link>
-    <Link to='/' className='nav-link'><img src={free_room} alt='search' /></Link>
-    <Link to='/' className='nav-link'><img src={map} alt='search' /></Link>
-    <Link to='/' className='nav-link'><img src={dark_mode} alt='search' /></Link> 
-  </div>
-</nav>
+  const [clicked, setClicked] = useState(false);
+
+  function handleClick() {
+    setClicked(!clicked);
+  }
+
+  return <nav className='navbar'>
+    <div className='navbar-brand'>
+      {/* handle click state change logic*/}
+      <Link to='/'>
+        <img 
+          src={clicked ? freeRoomsDoorClosed: freeRoomsLogo} 
+          alt='freeRoomsLogo'
+          onClick={handleClick}
+        />
+      Freerooms
+      </Link>
+    </div>
+    <div className='navbar-links'>
+      <Link to='/' className='nav-link'><img src={search} alt='search' /></Link>
+      <Link to='/' className='nav-link'><img src={grid_view} alt='search' /></Link>
+      <Link to='/' className='nav-link'><img src={free_room} alt='search' /></Link>
+      <Link to='/' className='nav-link'><img src={map} alt='search' /></Link>
+      <Link to='/' className='nav-link'><img src={dark_mode} alt='search' /></Link> 
+    </div>
+  </nav>
 }
 
 export default NavBar
